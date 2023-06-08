3c9678ba0aca25a2aae7560a736eceaa71ce4d28
Austin Gregory <austingregory82@gmail.com>
2023-06-08 17:58:23 -0600

    Has on-click capabilities. Has more data.

--- src/data/data.js
+++ src/data/data.js
@@ -1,30 +1,90 @@
 export const data = {
     name: "Boulder Toss",
-    image: "WardenShamanIcon.png",
+    image: "BoulderTossIcon.jpg",
     type: "ability",
+    abilityDetails: ["Instant", "8 sec cooldown", "15 yd range"],
     requirements: ["Requires Shaman"],
+    points: 1,
     description: "Hurl a boulder at your target, dealing physical damage and spawning an [Earthquake] at the point of impact.",
     children: [
         {
             name: "Shield Slam",
-            image: "WardenShamanIcon.png",
+            image: "ShieldSlamIcon.jpg",
             type: "ability",
+            abilityDetails: ["Instant", "12 sec cooldown", "Melee range"],
             requirements: ["Requires Shaman", "Requires Shield"],
-            description: "Slam your shield into your target, dealing damage and applying an effect based on the currently active [Elemental Shield]. Damage type is also based on the currently active [Elemental Shield] ([Rock Shield]: Physical, [Lava Shield]: Fire, [Storm Shield]: Nature, [Ice Shield]: Frost)\n\nIf [Rock Shield] is active, spawn an [Earthquake] under the target.\n\nIf [Lava Shield] is active, sear the target, leaving a burn which deals fire damage over 18 seconds.\n\nIf [Storm Shield] is active, your next [Lightning Bolt] or [Chain Lightning] is an instant cast, is free, and is empowered. Proc lasts 12 seconds.\n\nIf [Ice Shield] is active, shatter the enemy’s defenses, increasing the damage you deal to the target by 10% for 10 seconds.\n\nIf no [Elemental Shield] is active, no additional effect occurs and the damage is physical."
+            points: 1,
+            description: "Slam your shield into your target, dealing damage and applying an effect based on the currently active [Elemental Shield]. Damage type is also based on the currently active [Elemental Shield] ([Rock Shield]: Physical, [Lava Shield]: Fire, [Storm Shield]: Nature, [Ice Shield]: Frost)\n\nIf [Rock Shield] is active, spawn an [Earthquake] under the target.\n\nIf [Lava Shield] is active, sear the target, leaving a burn which deals fire damage over 24 seconds.\n\n If [Storm Shield] is active, automatically cast a [Lightning Bolt] or [Chain Lightning] (whichever was cast most recently) at the target.\n\nIf [Ice Shield] is active, shatter the enemy’s defenses, increasing the damage you deal to the target by 10% for 10 seconds.\n\nIf no [Elemental Shield] is active, no additional effect occurs and the damage is physical.",
+            children: [
+                {
+                    name: "Violent Lightning",
+                    image: "ViolentLightningIcon.jpg",
+                    type: "passive",
+                    requirements: ["Requires Shaman"],
+                    description: "Taking damage and casting abilities while [Storm Shield] is active has a 3/6% chance to automatically emit a [Chain Lightning] to the attacker or the target. 2 second internal CD on damage taken"
+                },
+                {
+                    name: "Ice Cold Water",
+                    image: "IceColdWaterIcon.jpg",
+                    type: "passive",
+                    requirements: ["Requires Shaman"],
+                    description: "Taking damage and casting abilities while [Ice Shield] is active has a chance to make your next [Healing Surge] an instant cast, free, and empowered. Proc lasts 12 seconds. 2 second internal CD on damage taken"
+                }
+            ]
         },
         {
             name: "Earth Shock",
-            image: "WardenShamanIcon.png",
+            image: "EarthShockIcon.jpg",
             type: "ability",
+            abilityDetails: ["Instant", "6 sec cooldown", "40 yd range"],
             requirements: ["Requires Shaman"],
-            description: "Instantly shocks the target with concussive force, causing nature damage. [Earth Shock] will consume all stacks of [Fulmination] (max 20) for additional damage. Blocks, Parries, Absorbs, and [Earthen Strike] all add a stack of [Fulmination]."
+            points: 1,
+            description: "Instantly shocks the target with concussive force, causing nature damage. [Earth Shock] will consume all stacks of [Fulmination] (max 20) for additional damage. Blocks, Parries, Absorbs, and [Earthen Strike] all add a stack of [Fulmination].",
+            children: [
+                {
+                    name: "Rumble",
+                    image: "RumbleIcon.jpg",
+                    type: "passive",
+                    requirements: ["Requires Shaman"],
+                    points: 1, 
+                    description: "When [Earth Shock] is used at 20 stacks of [Fulmination], an [Earthquake] is spawned at the target’s location and the target deals 5% reduced damage for 8 seconds"
+                },
+                {
+                    name: "Sundering",
+                    image: "SunderingIcon.jpg",
+                    type: "ability",
+                    abilityDetails: ["Instant", "40 sec cooldown"],
+                    requirements: ["Requires Shaman", "Requires Melee Weapon"],
+                    points: 1,
+                    description: "Shatters a line of earth in front of you with your main hand weapon, dealing Flamestrike damage and Incapacitating any enemy hit for 2 sec."
+                }
+            ]
         },
         {
             name: "Earthen Strike",
             image: "EarthenStrikeIcon.jpg",
             type: "ability",
+            abilityDetails: ["Instant", "3 sec cooldown", "Melee range"],
+            points: 1,
             requirements: ["Requires Shaman"],
             description: "Strike your target with your weapon, dealing a split between nature and physical damage. Grants a stack of [Fulmination].",
+            children: [
+                {
+                    name: "Hotheaded",
+                    image: "HotheadedIcon.jpg",
+                    type: "passive",
+                    requirements: ["Requires Shaman"],
+                    points: 2,
+                    description: "Taking damage and casting abilities while [Lava Shield] is active has a 4/6% chance to increase haste by 2/3% for 10 seconds, stacking up to 5 times. 2 second internal CD on damage taken."
+                },
+                {
+                    name: "Rock Splinters",
+                    image: "RockSplintersIcon.jpg",
+                    type: "ability",
+                    requirements: ["Requires Shaman"],
+                    description: "[Earthen Strike] hits 2 additional targets while you are in an [Earthquake]. Note that each target hit by [Earthen Strike] grants a stack of [Fulmination]."
+                }
+            ]
         }
     ]
 }
--- src/pages/talents.js
+++ src/pages/talents.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from 'react';
 
 //import {Tree} from "react-tree-graph";
 import Tree from "react-d3-tree";
@@ -20,46 +20,121 @@ const containerStyles = {
 const nodeSize = { x:56, y:56}
 const borderWidth = 4
 const foreignObjectProps = { width: nodeSize.x+borderWidth*2, height: nodeSize.y+borderWidth*2, x: -nodeSize.x/2-borderWidth, y: -nodeSize.y/2-borderWidth};
-const separation = { siblings: 2, nonSiblings: 2};
-const depthFactor = 100
-const collapsible = false
-const draggable = false
-const zoomable = false
+//const translate = {x: this.props.width, y: this.props.height}
 
-
-// const infoItems = ["line one", "line two"]
-// const tip = infoItems.join('\n')
-
-// <Tooltip
-//     title={
-//         <div style={{ whiteSpace: 'pre-line' }}>{tip}</div>
-//     }
-// >
-//     <IconButton/>
-// </Tooltip>
-//TODO update tooltip title to have new lines 
 const renderForeignObjectNode = ({
   nodeDatum,
   toggleNode,
   foreignObjectProps,
-  classes
+  handleNodeClick,
 }) => (
-  <Tooltip title={<div style={{whiteSpace: 'pre-line'}}>{nodeDatum.name + '\n' + nodeDatum.requirements.join('\n') + '\n' + nodeDatum.description}</div>} placement="right">
+  <Tooltip title={<div style={{whiteSpace: 'pre-line'}}>{generateTooltip(nodeDatum)}</div>} placement="right">
     <foreignObject {...foreignObjectProps}>
         <div style={{border: "4px solid limegreen", height: nodeSize.y, width: nodeSize.x}}>
-          <img src={nodeDatum.image}></img>
+          <img src={nodeDatum.image} onClick={() => handleNodeClick(nodeDatum, foreignObjectProps)}></img>
         </div>
     </foreignObject>
   </Tooltip>
 );
 
-const handleNodeClick = () => {
+const renderForeignObjectNodeChosen = ({
+  nodeDatum,
+  toggleNode,
+  foreignObjectProps,
+  handleNodeClick,
+}) => (
+  <Tooltip title={<div style={{whiteSpace: 'pre-line'}}>{generateTooltip(nodeDatum)}</div>} placement="right">
+    <foreignObject {...foreignObjectProps}>
+        <div style={{border: "4px solid yellow", height: nodeSize.y, width: nodeSize.x}}>
+          <img src={nodeDatum.image} onClick={() => handleNodeClick(nodeDatum, foreignObjectProps)}></img>
+        </div>
+    </foreignObject>
+  </Tooltip>
+);
 
+const customNodeFnMapping = {
+  available: {
+    fn: (nodeDatum, foreignObjectProps, handleNodeClick) => (
+      <Tooltip title={<div style={{whiteSpace: 'pre-line'}}>{generateTooltip(nodeDatum)}</div>} placement="right">
+        <foreignObject {...foreignObjectProps}>
+            <div style={{border: "4px solid limegreen", height: nodeSize.y, width: nodeSize.x}}>
+              <img src={nodeDatum.image} onClick={() => handleNodeClick(nodeDatum, foreignObjectProps)}></img>
+            </div>
+        </foreignObject>
+      </Tooltip>
+    ),
+  },
+  unavailable: {
+    fn: (nodeDatum, foreignObjectProps, handleNodeClick) => (
+      <Tooltip title={<div style={{whiteSpace: 'pre-line'}}>{generateTooltip(nodeDatum)}</div>} placement="right">
+        <foreignObject {...foreignObjectProps}>
+            <div style={{border: "4px solid blue", height: nodeSize.y, width: nodeSize.x}}>
+              <img src={nodeDatum.image} onClick={() => handleNodeClick(nodeDatum, foreignObjectProps)}></img>
+            </div>
+        </foreignObject>
+      </Tooltip>
+    ),
+  },
+  chosen: {
+    fn: (nodeDatum, foreignObjectProps, handleNodeClick) => (
+      <Tooltip title={<div style={{whiteSpace: 'pre-line'}}>{generateTooltip(nodeDatum)}</div>} placement="right">
+        <foreignObject {...foreignObjectProps}>
+            <div style={{border: "4px solid black", height: nodeSize.y, width: nodeSize.x}}>
+              <img src={nodeDatum.image} onClick={() => handleNodeClick(nodeDatum, foreignObjectProps)}></img>
+            </div>
+        </foreignObject>
+      </Tooltip>
+    ),
+  },
 };
 
-const Talents = () => {
+// const handleNodeClick = (nodeDatum, foreignObjectProps) => {
+//   console.log(nodeDatum.description)
+//   nodeDatum.description = "Ooga!"
+//   console.log(nodeDatum.description);
+//   handleCustomNodeFnChange()
+// };
 
-    const [translate, containerRef] = useCenteredTree();
+class TalentTree extends Component {
+  constructor() {
+      super();
+
+      this.addedNodesCount = 0;
+
+      this.state = {
+        data: data,
+        orientation: 'vertical',
+        collapsible: false,
+        zoomable: false,
+        draggable: false,
+        depthFactor: 100,
+        translateX: window.innerWidth/2,
+        translateY: window.innerWidth/16,
+        separation: { siblings: 2, nonSiblings: 2},
+        nodeSize: nodeSize,
+        renderCustomNodeElement: {renderForeignObjectNode},
+        //renderCustomNodeElement: customNodeFnMapping['available'].fn
+      };
+
+      //this.setTranslate = this.setTranslate.bind(this);
+      this.setPathFunc = this.setPathFunc.bind(this);
+  }
+  
+  setPathFunc(pathFunc) {
+    this.setState({ pathFunc })
+  }
+  // setTranslate() {
+  //   this.setState({ translate: useCenteredTree()})
+  // }
+
+  handleCustomNodeFnChange = evt => {
+    console.log('anyone there?')
+    //this.setState({ renderCustomNodeElement: customNodeFnMapping['chosen'].fn });
+    this.setState({ renderCustomNodeElement: renderForeignObjectNodeChosen});
+  };
+
+  render() {
+    //const [translate, containerRef] = useCenteredTree();
     const straightPathFunc = (linkDatum) => {
       const { source, target } = linkDatum;
       //Target Node is left of the source Node
@@ -74,44 +149,131 @@ const Talents = () => {
     };
 
     const getDynamicPathClass = ({ source, target }, orientation) => {
-      if (!target.children) {
-        // Target node has no children -> this link leads to a leaf node.
-        return 'link';
-      }
       // Style it as a link connecting two branch nodes by default.
-      return 'link';
+      return 'active_path';
     };
 
+    const handleNodeClick = (nodeDatum, foreignObjectProps) => {
+      console.log(nodeDatum.description)
+      nodeDatum.description = "Ooga!"
+      console.log(nodeDatum.description);
+      this.handleCustomNodeFnChange()
+    };
 
+    //goes inside <div style... ref={containerRef}>
     return (
-        <div style={containerStyles} ref={containerRef}>
-            <Tree
-                data={data}
-                collapsible={collapsible}
-                draggable={draggable}
-                onNodeClick={handleNodeClick}
-                zoomable={zoomable}
-                translate={translate}
-                nodeSize={nodeSize}
-                separation={separation}
-                depthFactor={depthFactor}
-                renderCustomNodeElement={(nodeProps) =>
-                    renderForeignObjectNode({ ...nodeProps, foreignObjectProps, data})
-                }
-                //pathFunc="straight"
-                pathFunc={straightPathFunc}
-                //pathClassFunc={getDynamicPathClass}
-                //pathClassFunc={() => 'path'}
-                orientation="vertical"
-                styles={{
-                  links: {
-                        stroke: 'red',
-                        strokeWidth: "2px",
-                      },
-               }}
-            />
+      <div style={containerStyles} >
+          <Tree 
+              data={this.state.data}
+              collapsible={this.state.collapsible}
+              draggable={this.state.draggable}
+              zoomable={this.state.zoomable}
+              translate={{x: this.state.translateX, y: this.state.translateY}}
+              nodeSize={this.state.nodeSize}
+              separation={this.state.separation}
+              depthFactor={this.state.depthFactor}
+              renderCustomNodeElement={(rd3tProps) =>
+                  renderForeignObjectNode({ ...rd3tProps, foreignObjectProps, handleNodeClick})
+              }
+              // renderCustomNodeElement={
+              //   this.state.renderCustomNodeElement
+              //     ? rd3tProps => this.state.renderCustomNodeElement(rd3tProps, this.state)
+              //     : undefined
+              // }
+              // renderCustomNodeElement={
+              //   this.state.renderCustomNodeElement
+              //     ? rd3tProps => this.state.renderCustomNodeElement(rd3tProps, this.state)
+              //     : undefined
+              // }
+              //pathFunc="straight"
+              pathFunc={straightPathFunc}
+              pathClassFunc={getDynamicPathClass}
+              //pathClassFunc={() => 'path'}
+              orientation="vertical"
+              //onNodeClick={(nodeDatum) => handleNodeClick(...nodeDatum)}
+              //onNodeClick={this.handleNodeClick}
+            
+          />
+      </div>
+  );
+  }
+}
+
+const generateTooltip = (nodeDatum) => {
+  const abilityDetails = nodeDatum.abilityDetails === undefined ? "" : nodeDatum.abilityDetails.join('\n') + '\n'
+  const name = nodeDatum.name + '\n'
+  const requirements = nodeDatum.requirements.join('\n') + '\n'
+  const description = nodeDatum.description
+  const tooltip = name + abilityDetails + requirements + description
+  return tooltip;
+}
+
+const renderForeignObjectNodeTest = ({
+  nodeDatum,
+  toggleNode,
+  foreignObjectProps,
+  handleNodeClick,
+}) => (
+  <Tooltip title={<div style={{whiteSpace: 'pre-line'}}>{generateTooltip(nodeDatum)}</div>} placement="right">
+    <foreignObject {...foreignObjectProps}>
+        <div style={{border: "4px solid limegreen", height: nodeSize.y, width: nodeSize.x}}>d
+          <img src={nodeDatum.image} onClick={handleNodeClick({nodeDatum, foreignObjectProps})}></img>
         </div>
-    );
-};
+    </foreignObject>
+  </Tooltip>
+);
+
+// const Talents = () => {
+
+//     const [translate, containerRef] = useCenteredTree();
+//     const straightPathFunc = (linkDatum) => {
+//       const { source, target } = linkDatum;
+//       //Target Node is left of the source Node
+//       if(source.x > target.x)
+//         return `M${source.x-nodeSize.x/2},${source.y+nodeSize.x/2}L${target.x},${target.y-nodeSize.y/2}`;
+//       //Target Node is right of the source Node
+//       else if(source.x < target.x)
+//         return `M${source.x+nodeSize.x/2},${source.y+nodeSize.x/2}L${target.x},${target.y-nodeSize.y/2}`;
+//       //Target Node is aligned with the source Node
+//       else
+//         return `M${source.x},${source.y}L${target.x},${target.y-nodeSize.y/2}`;
+//     };
+
+//     const getDynamicPathClass = ({ source, target }, orientation) => {
+//       // Style it as a link connecting two branch nodes by default.
+//       return 'active_path';
+//     };
+    
+//     const handleNodeClick = (nodeDatum, foreignObjectProps) => {
+//       window.alert(
+//         nodeDatum.children ? "Clicked a branch node" : "Clicked a leaf node."
+//       );
+//     };
+
+//     return (
+//         <div style={containerStyles} ref={containerRef}>
+//             <Tree 
+//                 data={data}
+//                 collapsible={collapsible}
+//                 draggable={draggable}
+//                 //onNodeClick={(nodeDatum) => handleNodeClick(...nodeDatum)}
+//                 zoomable={zoomable}
+//                 translate={translate}
+//                 renderCustomNodeElement={(rd3tProps) =>
+//                     renderForeignObjectNode({ ...rd3tProps, foreignObjectProps, handleNodeClick})
+//                 }
+//                 //pathFunc="straight"
+//                 pathFunc={straightPathFunc}
+//                 pathClassFunc={getDynamicPathClass}
+//                 //pathClassFunc={() => 'path'}
+//                 orientation="vertical"
+//             />
+//         </div>
+//     );
+// };        nodeSize={nodeSize}
+//                 separation={separation}
+//                 depthFactor={depthFactor}
+        
  
-export default Talents;
+//export default Talents;
+export default TalentTree;
